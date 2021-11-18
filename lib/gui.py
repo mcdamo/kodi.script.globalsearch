393350230051effe4b5c3d9a0570c23dedbaa01f
mcdamo <mcdamo@users.noreply.github.com>
2021-11-18 22:58:45 +1100

    Search settings, keyword search, fixes
    * Add addon setting for 'Hide Watched'
    * Add advanced search options
    * Fix nested search and nested item counts
    * Hide 'unwatched' icon from 'back' buttons
    * Fix missing 'back' folder icon on nested videos
    * Fix toggle unwatched reverts to top-level results
    * Fix nav_back from director's movies loads blank page

--- lib/gui.py
+++ lib/gui.py
@@ -1,6 +1,7 @@
 import datetime
 import json
 import operator
+import re
 from .defs import *
 
 def log(txt):
@@ -20,7 +21,6 @@ class GUI(xbmcgui.WindowXML):
         self.navback = False
         self.history = {}
         self.menuposition = 0
-        self.unwatched = False
         self.searchstring = self._clean_string(self.searchstring).strip()
         if self.searchstring == '':
             self._close()
@@ -41,7 +41,7 @@ class GUI(xbmcgui.WindowXML):
             self._fetch_items()
 
     def _hide_controls(self):
-        for cid in [SEARCHBUTTON, NORESULTS]:
+        for cid in (SEARCHBUTTON, NORESULTS):
             self.getControl(cid).setVisible(False)
 
     def _parse_argv(self):
@@ -52,6 +52,8 @@ class GUI(xbmcgui.WindowXML):
         for key, value in CATEGORIES.items():
             if key not in ('albumsongs', 'artistalbums', 'tvshowseasons', 'seasonepisodes', 'episodesplot', 'actormovies', 'directormovies', 'actortvshows'):
                 CATEGORIES[key]['enabled'] = ADDON.getSettingBool(key)
+        self.hidewatched = ADDON.getSettingBool('hidewatched')
+        self._set_hidewatched_label()
 
     def _get_preferences(self):
         json_query = xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "Settings.GetSettingValue", "params":{"setting":"myvideos.selectaction"}, "id": 1}')
@@ -104,19 +106,52 @@ class GUI(xbmcgui.WindowXML):
         self._check_focus()
 
     def _get_items(self, cat, search):
+        search_actors = []
+        search_directors = []
+        search_keywords = []
         if cat['content'] == 'livetv':
             self._fetch_channelgroups(cat)
             return
         if cat['type'] == 'seasonepisodes':
             search = search[0], search[1]
             rule = cat['rule'].format(query0 = search[0], query1 = search[1])
-        elif (cat['type'] == 'episodes' and ADDON.getSettingBool('episodesplot')) or (cat['type'] == 'movies' and ADDON.getSettingBool('moviesplot')):
-            rule = cat['ruleplot'].format(query = search)
+        elif cat['type'] in ('movies', 'tvshows', 'episodes', 'musicvideos', 'artists', 'albums', 'songs', 'actors', 'directors', 'tvactors'):
+            (required_rules, search_keywords, search_actors, search_directors) = self._parse_search(cat['filters'], search)
+            if len(search_keywords) == 0 and len(required_rules) == 0:
+                return
+            keywords_rule = ''
+            if search_keywords:
+                filters = []
+                filter = self._keyword_filters(cat['filters']['default'], search_keywords)
+                filters.append(filter)
+                if ('path' in cat['filters'] and ADDON.getSettingBool('paths')):
+                    filter = self._keyword_filters(cat['filters']['path'], search_keywords)
+                    filters.append(filter)
+                if ('filename' in cat['filters'] and ADDON.getSettingBool('filenames')):
+                    filter = self._keyword_filters(cat['filters']['filename'], search_keywords)
+                    filters.append(filter)
+                if ('plot' in cat['filters'] and ((cat['type'] in ('tvshows', 'episodes') and ADDON.getSettingBool('episodesplot')) or (cat['type'] == 'movies' and ADDON.getSettingBool('moviesplot')))):
+                    filter = self._keyword_filters(cat['filters']['plot'], search_keywords)
+                    filters.append(filter)
+                keywords_rule = filters[0] if len(filters) == 1 else '{{"or":[{filters}]}}'.format(filters = ','.join(filters))
+            if required_rules:
+                if keywords_rule:
+                    required_rules.append(keywords_rule)
+                final_rule = required_rules[0] if len(required_rules) == 1 else '{{"and":[{filters}]}}'.format(filters = ','.join(required_rules))
+            else:
+                final_rule = keywords_rule
+            rule = cat['rule'].format(query = final_rule)
         else:
             rule = cat['rule'].format(query = search)
-        if (self.unwatched and (cat['type'] == 'episodes' or cat['type'] == 'movies')):
-            rule = rule[:9] + '{"and": [{"field": "playcount", "operator": "is", "value": "0"}, ' + rule[9:] + ']}'
-            log("Unwatched filter: {}".format(rule))
+        log("Type '{}' Rule: {}".format(cat['type'], rule))
+        if self.hidewatched:
+            if cat['type'] in ('episodes', 'movies', 'tvshows'):
+                rule = rule[:9] + '{"and": [{"field": "playcount", "operator": "is", "value": "0"}, ' + rule[9:] + ']}'
+                log("HideWatched Rule: {}".format(rule))
+            elif cat['type'] == 'seasonepisodes':
+                rule = rule + ', "filter": {"field": "playcount", "operator": "is", "value": "0"}'
+                log("HideWatched Rule: {}".format(rule))
+
         self.getControl(SEARCHCATEGORY).setLabel(xbmc.getLocalizedString(cat['label']))
         self.getControl(SEARCHCATEGORY).setVisible(True)
         json_query = xbmc.executeJSONRPC('{"jsonrpc":"2.0", "method":"%s", "params":{"properties":%s, "sort":{"method":"%s"}, %s}, "id": 1}' % (cat['method'], json.dumps(cat['properties']), cat['sort'], rule))
@@ -130,9 +165,10 @@ class GUI(xbmcgui.WindowXML):
             listitems.append(listitem)
         if 'result' in json_response and(json_response['result'] != None) and cat['content'] in json_response['result']:
             for item in json_response['result'][cat['content']]:
-                if cat['type'] == 'actors' or cat['type'] == 'tvactors':
+                if cat['type'] in ('actors', 'tvactors'):
                     for item in item['cast']:
-                        if search.lower() in item['name'].lower():
+                        name = item['name'].lower()
+                        if any(s in name for s in search_actors) or any(s in name for s in search_keywords):
                             name = item['name']
                             if 'thumbnail' in item:
                                 thumb = item['thumbnail']
@@ -147,7 +183,8 @@ class GUI(xbmcgui.WindowXML):
                             actors[name] = val
                 elif cat['type'] == 'directors':
                     for item in item['director']:
-                        if search.lower() in item.lower():
+                        name = item.lower()
+                        if any(s in name for s in search_directors) or any(s in name for s in search_keywords):
                             name = item
                             val = {}
                             val['thumb'] = cat['icon']
@@ -166,7 +203,7 @@ class GUI(xbmcgui.WindowXML):
                         listitem.addStreamInfo('audio', stream)
                     for stream in item['streamdetails']['subtitle']:
                         listitem.addStreamInfo('subtitle', stream)
-                if cat['type'] != 'actors' and cat['type'] != 'directors' and cat['type'] != 'tvactors':
+                if cat['type'] not in ('actors', 'directors', 'tvactors'):
                     listitem.setProperty('content', cat['content'])
                 if cat['content'] == 'tvshows' and cat['type'] != 'tvactors':
                     listitem.setProperty('TotalSeasons', str(item['season']))
@@ -174,10 +211,13 @@ class GUI(xbmcgui.WindowXML):
                     listitem.setProperty('WatchedEpisodes', str(item['watchedepisodes']))
                     listitem.setProperty('UnWatchedEpisodes', str(item['episode'] - item['watchedepisodes']))
                 elif cat['content'] == 'seasons':
+                    if (self.hidewatched and item['playcount'] == 1):
+                        # filter watched seasons, this is not possible in the query
+                        continue
                     listitem.setProperty('tvshowid', str(item['tvshowid']))
-                elif (cat['content'] == 'movies' and cat['type'] != 'actors' and cat['type'] != 'directors') or cat['content'] == 'episodes' or cat['content'] == 'musicvideos':
+                elif (cat['content'] == 'movies' and cat['type'] not in ('actors', 'directors')) or cat['content'] == 'episodes' or cat['content'] == 'musicvideos':
                     listitem.setProperty('resume', str(int(item['resume']['position'])))
-                elif cat['content'] == 'artists' or cat['content'] == 'albums':
+                elif cat['content'] in ('artists', 'albums'):
                     info, props = self._split_labels(item, cat['properties'], cat['content'][0:-1] + '_')
                     for key, value in props.items():
                         listitem.setProperty(key, value)
@@ -186,14 +226,14 @@ class GUI(xbmcgui.WindowXML):
                 if cat['content'] == 'songs':
                     listitem.setProperty('artistid', str(item['artistid'][0]))
                     listitem.setProperty('albumid', str(item['albumid']))
-                if (cat['content'] == 'movies' and cat['type'] != 'actors' and cat['type'] != 'directors') or (cat['content'] == 'tvshows' and cat['type'] != 'tvactors') or cat['content'] == 'episodes' or cat['content'] == 'musicvideos' or cat['content'] == 'songs':
+                if (cat['content'] == 'movies' and cat['type'] not in ('actors', 'directors')) or (cat['content'] == 'tvshows' and cat['type'] != 'tvactors') or cat['content'] == 'episodes' or cat['content'] == 'musicvideos' or cat['content'] == 'songs':
                     listitem.setPath(item['file'])
                 if cat['media']:
                     listitem.setInfo(cat['media'], self._get_info(item, cat['content'][0:-1]))
                     listitem.setProperty('media', cat['media'])
                 if cat['content'] == 'tvshows' and cat['type'] != 'tvactors':
                     listitem.setIsFolder(True)
-                if cat['type'] != 'actors' and cat['type'] != 'directors' and cat['type'] != 'tvactors':
+                if cat['type'] not in ('actors', 'directors', 'tvactors'):
                     listitems.append(listitem)
             if actors:
                 for name, val in sorted(actors.items()):
@@ -208,15 +248,17 @@ class GUI(xbmcgui.WindowXML):
                     listitem.setProperty('content', cat['type'])
                     listitems.append(listitem)
         if len(listitems) > 0:
-            if cat['type'] != 'actors' and cat['type'] != 'tvactors': 
-                menuitem = xbmcgui.ListItem(xbmc.getLocalizedString(cat['label']), str(len(listitems)), offscreen=True)
+            # exclude top '..' item from nested items count
+            listlen = len(listitems) - 1 if listitems[0].getLabel() == '..' else len(listitems)
+            if cat['type'] != 'actors' and cat['type'] != 'tvactors':
+                menuitem = xbmcgui.ListItem(xbmc.getLocalizedString(cat['label']), str(listlen), offscreen=True)
             else:
-                menuitem = xbmcgui.ListItem(LANGUAGE(cat['label']), str(len(listitems)), offscreen=True)
+                menuitem = xbmcgui.ListItem(LANGUAGE(cat['label']), str(listlen), offscreen=True)
             menuitem.setArt({'icon':cat['menuthumb']})
             menuitem.setProperty('type', cat['type'])
-            if cat['type'] != 'actors' and cat['type'] != 'directors' and cat['type'] != 'tvactors':
+            if cat['type'] not in ('actors', 'directors', 'tvactors'):
                 menuitem.setProperty('content', cat['content'])
-            elif cat['type'] == 'actors' or cat['type'] == 'tvactors':
+            elif cat['type'] in ('actors', 'tvactors'):
                 menuitem.setProperty('content', 'actors')
             elif cat['type'] == 'directors':
                 menuitem.setProperty('content', 'directors')
@@ -226,9 +268,9 @@ class GUI(xbmcgui.WindowXML):
             self.content[cat['type']] = listitems
             if self.navback and self.focusset == 'false':
                 if self.history[self.level]['menutype'] == cat['type']:
-                    if cat['type'] != 'actors' and cat['type'] != 'directors' and cat['type'] != 'tvactors':
+                    if cat['type'] not in ('actors', 'directors', 'tvactors'):
                         self.setContent(cat['content'])
-                    elif cat['type'] == 'actors' or cat['type'] == 'tvactors':
+                    elif cat['type'] in ('actors', 'tvactors'):
                         self.setContent('actors')
                     elif cat['type'] == 'directors':
                         self.setContent('directors')
@@ -237,9 +279,9 @@ class GUI(xbmcgui.WindowXML):
                     self.menutype = cat['type']
                     self.focusset = 'true'
             elif self.focusset == 'false':
-                if cat['type'] != 'actors' and cat['type'] != 'directors' and cat['type'] != 'tvactors':
+                if cat['type'] not in ('actors', 'directors', 'tvactors'):
                     self.setContent(cat['content'])
-                elif cat['type'] == 'actors' or cat['type'] == 'tvactors':
+                elif cat['type'] in ('actors', 'tvactors'):
                     self.setContent('actors')
                 elif cat['type'] == 'directors':
                     self.setContent('directors')
@@ -247,6 +289,49 @@ class GUI(xbmcgui.WindowXML):
                 self.setFocusId(self.getCurrentContainerId())
                 self.menutype = cat['type']
                 self.focusset = 'true'
+
+    def _parse_search(self, filters, search):
+        """Parse search string for named parameters and phrases
+
+        Words separated by spaces are treated as keywords
+
+        Phrases are wrapped in double quotes
+        eg: "Doctor Who"
+
+        Named parameters are any of path, filename, title, or plot:
+        eg: path:tvshows
+            title:"Doctor Who"
+
+        Named parameters are mandatory in the search results and all keywords must match.
+        """
+        matches = re.findall(r'(((path|filename|title|plot|director|actor):)?([^\s"]+|"([^"]+)"))', search)
+        keywords = []
+        required = []
+        actors = []
+        directors = []
+        for match in matches:
+            # match[4] is phrase inside quotes, match[3] is any keyword
+            # eg. 'Doctor Who' or '"Doctor Who"'
+            keyword = match[4] or match[3]
+            if match[2]:
+                # match is a named parameter
+                if match[2] in filters:
+                    if match[2] == 'actor':
+                        actors.append(keyword)
+                    elif match[2] == 'director':
+                        directors.append(keyword)
+                    required.append(filters[match[2]].format(query = keyword))
+                else:
+                    # recognised param is not valid for this media type, so ignore
+                    pass
+            else:
+                keywords.append(keyword)
+        return (required, keywords, actors, directors)
+
+    def _keyword_filters(self, filter, keywords):
+        """Wrap filters in 'and' if there a multiple keywords"""
+        filters = list(map(lambda x: filter.format(query = x), keywords))
+        return filters[0] if len(filters) == 1 else '{{"and":[{filters}]}}'.format(filters = ','.join(filters))
 
     def _fetch_channelgroups(self, cat):
         self.getControl(SEARCHCATEGORY).setLabel(xbmc.getLocalizedString(19069))
@@ -429,7 +514,7 @@ class GUI(xbmcgui.WindowXML):
         elif key == 'albumsongs':
             search = listitem.getMusicInfoTag().getDbId()
         elif key == 'actormovies' or key == 'directormovies' or key == 'actortvshows':
-            search = listitem.getLabel()
+            search = '"' + listitem.getLabel() + '"'
         self._reset_variables()
         self._hide_controls()
         self.clearList()
@@ -636,18 +721,25 @@ class GUI(xbmcgui.WindowXML):
             self.clearList()
             self.onInit()
 
-    def _toggle_unwatched(self):
-        self.unwatched = not self.unwatched
-        log("Toggle unwatched {}".format(self.unwatched))
-        labelid = 16101 if self.unwatched else 16100 # "Unwatched" / "All videos"
-        self.getControl(TOGGLE_UNWATCHED).setLabel(xbmc.getLocalizedString(labelid))
+    def _set_hidewatched_label(self):
+        labelid = 16101 if self.hidewatched else 16100 # "Unwatched" / "All videos"
+        self.getControl(TOGGLE_HIDEWATCHED).setLabel(xbmc.getLocalizedString(labelid))
+
+    def _toggle_hidewatched(self):
+        self.hidewatched = not self.hidewatched
+        log("Toggle hidewatched {}".format(self.hidewatched))
+        self._set_hidewatched_label()
+
+        # refresh page and run search, similar to _nav_back()
         self._reset_variables()
-        self._init_items()
         self._hide_controls()
         self.clearList()
         self.menu.reset()
         self.oldfocus = 0
-        self._fetch_items()
+        cats = self.history[self.level]['cats']
+        search = self.history[self.level]['search']
+        for cat in cats:
+            self._get_items(cat, search)
 
     def onClick(self, controlId):
         if controlId == self.getCurrentContainerId():
@@ -657,6 +749,7 @@ class GUI(xbmcgui.WindowXML):
             if listitem.getLabel() == '..':
                 self.level -= 1
                 self._nav_back()
+                return
             if listitem.getVideoInfoTag().getMediaType():
                 media = listitem.getVideoInfoTag().getMediaType()
             elif listitem.getMusicInfoTag().getMediaType():
@@ -701,8 +794,8 @@ class GUI(xbmcgui.WindowXML):
             self._update_list(item, content)
         elif controlId == SEARCHBUTTON:
             self._new_search()
-        elif controlId == TOGGLE_UNWATCHED:
-            self._toggle_unwatched()
+        elif controlId == TOGGLE_HIDEWATCHED:
+            self._toggle_hidewatched()
 
     def onAction(self, action):
         if action.getId() in ACTION_CANCEL_DIALOG:
